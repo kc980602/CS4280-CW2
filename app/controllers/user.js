377805833a8d07adaf4eec11acefbdbb4ec5e19a
Kevin Cheung <cheung.puilam15@gmail.com>
2019-04-05 03:12:02 +0800

    user

---
+++ app/controllers/user.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var createError = require('http-errors');
+var modelUser = require('../models/user');
+//used as controllerLocation.list in route index.js
+//home page accessing location list without specific parameter
+module.exports.list = function(req, res, next) {
+    modelLocation(function(err, result) {
+        //just use callback to get back data from model, no parameter needed
+        if (err) return next(createError(err)); //normal error handling
+        res.render('index', result);
+    });
+};
+//used as controllerLocation.info in route location.js
+//details page of a location
+
+module.exports.info = function(req, res, next) {
+    modelLocation(req.query.id, function(err, result) {
+        //assume parameter location id coming from query string
+        if (err) return next(createError(err)); //normal error handling
+        //send data to view of details page
+        res.render('details', result);
+    });
+};
---
+++ app/routes/users.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var controllerUser = require('../controllers/user')
+
+router.get('/', controllerUser.info());
+
+
+
+module.exports = router;
