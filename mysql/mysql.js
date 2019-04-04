898f3865b011fffc92bfd278b56b3163e4f70736
Kevin Cheung <cheung.puilam15@gmail.com>
2019-04-05 00:02:51 +0800

    init

---
+++ mysql/mysql.js
@@ -0,0 +1,8 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    "host": "cs4280-cw2.cvnekcb5vorc.us-east-2.rds.amazonaws.com",
+    "port": 3306,
+    "user": "admin",
+    "password": "UT56WbfuPRZTG89",
+    "database": "dbname"
+});
