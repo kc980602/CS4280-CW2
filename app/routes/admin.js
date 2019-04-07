925d425c1e39e9f185480e14836b41e52423cfda
Kevin Cheung <cheung.puilam15@gmail.com>
2019-04-07 23:08:38 +0800

    admin page UI

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,20 +2,24 @@ var express = require('express');
 var router = express.Router();
 // var controllerUser = require('../controllers/user')
 
+router.get('/', (req, res, next) => {
+    res.render('admin/index', { title: 'Browse Albums | Mue' });
+});
+
 router.get('/product', (req, res, next) => {
-    res.render('albums', { title: 'Browse Albums | Mue' });
+    res.render('admin/product', { title: 'Browse Albums | Mue' });
+});
+
+router.get('/product/add', (req, res, next) => {
+    res.render('admin/product-add', { title: 'Browse Albums | Mue' });
 });
 
 router.get('/product/album/:id', (req, res, next) => {
-    res.render('album', { title: 'Browse Albums | Mue' });
-});
-
-router.get('/product/album/add', (req, res, next) => {
-    res.render('album', { title: 'Browse Albums | Mue' });
+    res.render('admin/index', { title: 'Browse Albums | Mue' });
 });
 
 router.get('/product/refund', (req, res, next) => {
-    res.render('album', { title: 'Browse Albums | Mue' });
+    res.render('admin/refund', { title: 'Browse Albums | Mue' });
 });
 
 
