ef6e9241447c836c59227ee5d3935f73015f492d
Masterk <masterk7226@gmail.com>
2019-04-20 17:22:46 +0800

    update mysql getconnection

--- mysql/utils.js
+++ mysql/utils.js
@@ -4,13 +4,40 @@ async function insert() {
 
 }
 
-async function query(query, field) {
-    try {
-        const result = await pool.query(query, field)
-        return result
-    } catch(err) {
-        throw new Error(err)
-    }
+function query(query, fields) {
+    return new Promise((resolve) => {
+        try {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    switch (err.code) {
+                        case 'PROTOCOL_CONNECTION_LOST':
+                            console.error('Database connection was closed.')
+                        case 'ER_CON_COUNT_ERROR':
+                            console.error('Database has too many connections.')
+                        case 'ECONNREFUSED':
+                            console.error('Database connection was refused.')
+                        default:
+                            console.error(err);
+                    }
+                    connection.release();
+
+                    resolve(null);
+                }
+
+                connection.query(query, fields, function (err, results) {
+                    connection.release();
+
+                    if (err) {
+                        resolve(null);
+                    }
+
+                    resolve(results);
+                });
+            })
+        } catch (err) {
+            throw new Error(err)
+        }
+    });
 }
 
 module.exports = {
